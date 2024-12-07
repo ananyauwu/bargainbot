7f62f5e386ace4cdabdc3da02e998149ca201e33
ananyauwu <89239505+ananyauwu@users.noreply.github.com>
2024-12-07 20:54:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,5 +130,5 @@ def index():
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
